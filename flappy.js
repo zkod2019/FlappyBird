091516f7e4b1a49bd89c520138ed3aa02d47c363
ZKod <zkod777@gmail.com>
2022-09-07 14:22:32 +0400

    first commit

---
+++ flappy.js
@@ -0,0 +1,38 @@
+var robot;
+var block = [];
+
+function setup() {
+  createCanvas(640, 480);
+  robot = new Robot();
+  block.push(new Block());
+}
+
+function draw() {
+  background(0);
+
+  for (var i = block.length-1; i >= 0; i--) {
+    block[i].show();
+    block[i].update();
+
+    if (block[i].hits(robot)) {
+      console.log("HIT");
+    }
+
+    if (block[i].offscreen()) {
+        block.splice(i, 1);
+    }
+  }
+
+  robot.update();
+  robot.show();
+
+  if (frameCount % 75 == 0) {
+    block.push(new Pipe());
+  }
+}
+
+function keyPressed() {
+  if (key == ' ') {
+    robot.up();
+  }
+}
